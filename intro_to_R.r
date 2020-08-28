a675ee94b3744df133d08729fa6273fad839dbbc
_sandarbhTiwari <tiwari.sandarbh2603@gmail.com>
2020-08-28 17:17:25 +0530

    variable declaration, print() and cat() in R

--- intro_to_R.r
+++ intro_to_R.r
@@ -1,4 +1,7 @@
 # variable in R 
 # defining a variable x which will hold value 10
 x = 10;
-print(x)
+print(x);
+# another way to define a variable in R
+var_y <- 20;
+cat("Value of Y is= ", var_y,"\n");
